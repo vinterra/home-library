3a3d4495b6785c3921441b022a19a1214a262482
valentina.marioli <valentina.marioli@mycompany.com>
2016-09-13 15:07:59 +0000

    Fix to zipUtil

--- src/main/java/org/gcube/common/homelibrary/util/zip/ZipUtil.java
+++ src/main/java/org/gcube/common/homelibrary/util/zip/ZipUtil.java
@@ -1,12 +1,11 @@
-/**
- * 
- */
 package org.gcube.common.homelibrary.util.zip;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -15,6 +14,7 @@ import org.apache.commons.io.IOUtils;
 import org.gcube.common.homelibrary.home.exceptions.InternalErrorException;
 import org.gcube.common.homelibrary.home.workspace.WorkspaceFolder;
 import org.gcube.common.homelibrary.home.workspace.WorkspaceItem;
+import org.gcube.common.homelibrary.home.workspace.folder.items.ExternalFile;
 import org.gcube.common.homelibrary.home.workspace.folder.items.GCubeItem;
 import org.gcube.common.homelibrary.home.workspace.folder.items.ts.TimeSeries;
 import org.gcube.common.homelibrary.util.zip.zipmodel.ZipItem;
@@ -38,7 +38,7 @@ public class ZipUtil {
 	 */
 	public static File zipFolder(WorkspaceFolder folder) throws IOException, InternalErrorException
 	{
-		return zipWorkspaceItem(folder, false, null);
+		return zipWorkspaceItem(folder);
 	}
 
 
@@ -51,10 +51,8 @@ public class ZipUtil {
 	 */
 	public static File zipDocument(GCubeItem document) throws IOException, InternalErrorException
 	{
-		return zipWorkspaceItem(document, false, null);
+		return zipWorkspaceItem(document);
 	}
-
-
 
 
 	/**
@@ -65,7 +63,7 @@ public class ZipUtil {
 	 */
 	public static File zipTimeSeries(TimeSeries ts) throws IOException, InternalErrorException
 	{
-		return zipWorkspaceItem(ts, false, null);
+		return zipWorkspaceItem(ts);
 	}
 
 
@@ -107,7 +105,7 @@ public class ZipUtil {
 	 */
 	public static File zipDocument(GCubeItem document, boolean skipRoot) throws IOException, InternalErrorException
 	{
-		return zipWorkspaceItem(document, skipRoot, null);
+		return zipWorkspaceItem(document, skipRoot);
 	}
 
 
@@ -121,10 +119,53 @@ public class ZipUtil {
 	 */
 	public static File zipTimeSeries(TimeSeries ts, boolean skipRoot) throws IOException, InternalErrorException
 	{
-		return zipWorkspaceItem(ts, skipRoot, null);
+		return zipWorkspaceItem(ts, skipRoot);
 	}
 
+	
+	protected static File zipWorkspaceItem(WorkspaceItem workspaceItem) throws InternalErrorException, IOException
+	{
+//		return zipWorkspaceItem(workspaceItem, false);
+		File zipFile = File.createTempFile("zippping", "gz");
+		try (ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(zipFile))) {
+			processFolder(workspaceItem, zipOutputStream,  workspaceItem.getPath().length() + 1);
+		}
+		return zipFile;		
+	}
+	
 
+
+/**
+ * Simple zip folder
+ * @param myFolder
+ * @param zipOutputStream
+ * @param prefixLength
+ * @throws IOException
+ * @throws InternalErrorException
+ */
+	private static void processFolder(WorkspaceItem myFolder, final ZipOutputStream zipOutputStream, final int prefixLength)
+			throws IOException, InternalErrorException {
+		for (WorkspaceItem file: myFolder.getChildren()) {
+			if (!file.isFolder()) {
+				final ZipEntry zipEntry = new ZipEntry(file.getPath().substring(prefixLength));
+				zipOutputStream.putNextEntry(zipEntry);
+				ExternalFile externalFile = (ExternalFile)file;
+				try (InputStream inputStream = externalFile.getData()) {
+					IOUtils.copy(inputStream, zipOutputStream);
+				}
+				zipOutputStream.closeEntry();
+			} else {
+				processFolder(file, zipOutputStream, prefixLength);
+			}
+		}
+	}
+	
+	protected static File zipWorkspaceItem(WorkspaceItem workspaceItem, boolean skipRoot) throws InternalErrorException, IOException
+	{
+		return zipWorkspaceItem(workspaceItem, skipRoot, new ArrayList<String>());
+	}
+	
+	
 	protected static File zipWorkspaceItem(WorkspaceItem workspaceItem, boolean skipRoot, List<String> idsToExclude) throws InternalErrorException, IOException
 	{
 		logger.trace("Zipping "+workspaceItem);
@@ -133,8 +174,8 @@ public class ZipUtil {
 		WorkspaceToZipModelConverter zipConverter = new WorkspaceToZipModelConverter();
 		ZipItem item = zipConverter.convert(workspaceItem, idsToExclude);
 
-		ZipModelVisitor zipModelVisitor = new ZipModelVisitor();
-		zipModelVisitor.visitItem(item);
+//		ZipModelVisitor zipModelVisitor = new ZipModelVisitor();
+//		zipModelVisitor.visitItem(item);
 
 		logger.trace("writing model");
 		ZipModelWriter zipModelWriter = new ZipModelWriter();
@@ -146,14 +187,15 @@ public class ZipUtil {
 
 	protected static File zipWorkspaceItem(List<WorkspaceItem> items, List<String> idsToExclude) throws InternalErrorException, IOException
 	{
+		
 		logger.trace("Zipping items: "+items.toString());
 
 		logger.trace("converting to zip model");
 		WorkspaceToZipModelConverter zipConverter = new WorkspaceToZipModelConverter();
 		ZipItem item = zipConverter.convert(items, idsToExclude);
 
-		ZipModelVisitor zipModelVisitor = new ZipModelVisitor();
-		zipModelVisitor.visitItem(item);
+//		ZipModelVisitor zipModelVisitor = new ZipModelVisitor();
+//		zipModelVisitor.visitItem(item);
 
 		logger.trace("writing model");
 		ZipModelWriter zipModelWriter = new ZipModelWriter();
